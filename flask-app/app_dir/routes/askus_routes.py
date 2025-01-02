62ba2a66c6979992a04366e4e89d6d3141639879
gregoryhornyak <hornyakj@uni.coventry.ac.uk>
2025-01-02 14:11:36 +0100

    new login - user account system to limit votes

--- flask-app/app_dir/routes/askus_routes.py
+++ flask-app/app_dir/routes/askus_routes.py
@@ -1,18 +1,33 @@
-from flask import Blueprint, render_template, request, redirect, jsonify, url_for
-from app_dir.utils.askus_utils import load_question, load_scores, save_scores, get_question, save_questions, get_daily_question
+from flask import Blueprint, render_template, request, redirect, jsonify, url_for, session
+from app_dir.utils.askus_utils import load_question, load_scores, save_scores, get_question, save_questions, get_daily_question,load_user_status, save_player_stat
 
 bp = Blueprint('askus', __name__, template_folder='templates')
 
-@bp.route("/", methods=['GET', 'POST'])
+@bp.route("/")
+def index():
+    return redirect(url_for("askus.login"))
+
+
+@bp.route("/poll", methods=['GET', 'POST'])
 def main():
     submitted = False
     question = get_daily_question()
     scores = load_scores()
     options = scores.keys()
     results = {}
+    stats = load_user_status()
+    print(f"{stats = }")
     vote_count = sum(scores.values())
     player_count = len(scores)
-    if request.method == 'POST':
+    user = session["user"]
+    print(f"{user = }")
+    if request.method == 'GET':    
+        print(f"{stats[user] = }")
+        if stats[user] == 2: # already voted
+            submitted = True
+        results = scores
+        vote_count = sum(scores.values())
+    elif request.method == 'POST':
         if vote_count == player_count:
             results = scores
             vote_count = sum(scores.values())
@@ -22,18 +37,24 @@ def main():
             scores[choice] += 1
             save_scores(scores)
             submitted = True
+            stats[user] = 2 # submitted vote
+            print(f"new value of {stats[user] = }")
+            save_player_stat(stats)
             results = scores
             vote_count = sum(scores.values())
 
     return render_template('askus.html', question=question, options=options, vote_count=vote_count, results=results, form_submitted=submitted, player_num=player_count)
 
-@bp.route('/scores')
-def scores():
-    return jsonify(load_scores())
-
-@bp.route('/board')
-def board():
-    return render_template('askus2.html')
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    options = load_user_status()
+    if request.method == "POST":
+        choice = request.form['vote']
+        #options[choice] = 1 # logged in
+        #save_player_stat(options)
+        session["user"] = choice
+        return redirect(url_for("askus.main"))
+    return render_template('askus_login.html',options=options)
 
 @bp.route('/reset')
 def reset_scores():
