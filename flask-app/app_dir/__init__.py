8a17962a9a504cd63eb9e946588be6ca2e7a5620
gregoryhornyak <hornyakj@uni.coventry.ac.uk>
2025-01-04 17:21:59 +0100

    logging

--- flask-app/app_dir/__init__.py
+++ flask-app/app_dir/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from .routes import main_routes, askus_routes, gamechanger_routes
-import random
-import string
+import logging
 
 def create_app():
+    time_format = "%Y-%m-%d %H:%M:%S"
+    logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format='[%(asctime)s] %(levelname)s %(name)s : %(message)s', datefmt=time_format)
     app = Flask(__name__)
     with open("flask_secret","r") as f:
         stringg = f.readline()
     app.config['SECRET_KEY'] = stringg#"mpuPR4CrqCOiPsse"#''.join(random.choices(string.ascii_letters + string.digits, k=32))
-
+    app.logger.debug("Secret key aquired")
     # Register blueprints
     app.register_blueprint(main_routes.bp)
     app.register_blueprint(askus_routes.bp, url_prefix="/askus")
