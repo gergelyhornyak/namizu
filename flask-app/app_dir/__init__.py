65dd69b2f7319baafa7d4484e89e915942ceeb73
gregoryhornyak <hornyakj@uni.coventry.ac.uk>
2025-02-16 15:10:15 +0000

    removed gamechanger link

--- flask-app/app_dir/__init__.py
+++ flask-app/app_dir/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from .routes import main_routes, namizu_routes, gamechanger_routes
+from .routes import main_routes, namizu_routes
 import logging
 
 def create_app():
     time_format = "%Y-%m-%d %H:%M:%S"
-    logging.basicConfig(filename='database/logfile.log', level=logging.DEBUG, format='[%(asctime)s] %(levelname)s %(name)s : %(message)s', datefmt=time_format)
+    #logging.basicConfig(filename='database/logfile.log', level=logging.DEBUG, format='[%(asctime)s] %(levelname)s %(name)s : %(message)s', datefmt=time_format)
     app = Flask(__name__)
     with open("flask_secret","r") as f:
         stringg = f.readline()
@@ -15,6 +15,5 @@ def create_app():
     # Register blueprints
     app.register_blueprint(main_routes.bp)
     app.register_blueprint(namizu_routes.bp, url_prefix="/namizu")
-    app.register_blueprint(gamechanger_routes.bp, url_prefix="/gamechanger")
 
     return app
