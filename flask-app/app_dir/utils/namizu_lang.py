46663949d1ed7863969431f0dc7608b51e19bb74
gregoryhornyak <hornyakj@uni.coventry.ac.uk>
2025-04-10 14:54:52 +0100

    localization

---
+++ flask-app/app_dir/utils/namizu_lang.py
@@ -0,0 +1,25 @@
+# language packs
+
+language_pack = {
+    "HU":{
+        "namizu.welcome":{
+            "title01":"Üdvözöllek a naMizun!",
+            "title02":"Heves szavazások barátok közt",
+            "title03":"-ként vagy belépve.",
+            "trio-info":"Nyílt-forráskódú, Reklám-mentes, Semmi átverés",
+            "poll_header":"Készen állsz egy szavazásra?",
+            "editor_header":"Szeretnél még több kérdést látni?",
+            "special_thanks":"Külön köszönet",
+            "footer":"A közösség erejével készült."
+        }
+    }
+}
+
+def load_hu(page):
+
+    if page == "namizu.welcome":
+        return
+    pass
+
+def load_eng(page):
+    pass
